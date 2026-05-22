0650ef4fe774ef856abc4c0c66e1624409ce10e6
Julio Rodríguez <juliorodriguez@vdenergy.es>
2026-05-22 21:30:10 +0200

    feat(backend): bootstrap spring boot and basic health check

---
+++ backend/src/test/java/com/vdenergy/inventory/InventoryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vdenergy.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
